70bd45a3ba58d2cd851f7a619e7b41776701dffc
Nikita Volokitin <n.a.volokitin@gmail.com>
2023-12-16 15:45:18 +0500

    Add a function page_vacancies_single

--- app/vacancies/views.py
+++ app/vacancies/views.py
@@ -14,5 +14,23 @@ vacancies_dao = VacanciesDAO('./data/vacancies.json')
 
 @vacancies_blueprint.route('/vacancies/')
 def page_vacancies():
+    """
+    Возвращает страницу со всеми вакансиями.
+
+    Возвращаемое значение:
+        str: Информация о всех вакансиях.
+    """
     vacancies = vacancies_dao.get_all()
     return render_template('vacancies_index.html', vacancies=vacancies)
+
+
+@vacancies_blueprint.route('/vacancies/<int:pk>')
+def page_vacancies_single(pk) -> str:
+    """
+    Возвращает страницу с одной вакансией.
+
+    Возвращаемое значение:
+        str: Информация о вакансии.
+    """
+    vacancy = vacancies_dao.get_by_pk(pk)
+    return render_template('vacancies_single.html', vacancy=vacancy)
